e06b6c23009b5f0cee59ea5b8e8a3cf79613a5d1
Vittorio D'Alfonso <50889805+GrandeVx@users.noreply.github.com>
2020-04-29 14:58:34 +0200

    Add files via upload

---
+++ Victory_SearchJob/client/client.lua
@@ -0,0 +1,6 @@
+RegisterNetEvent("output") -- Client Event
+AddEventHandler("output", function(argument)  -- If Event Was Called
+      if argument == 'police' then  -- Check the User Job // argument = User Job
+    TriggerEvent("chatMessage", "[Server]", {0,255,0}, "Sei Un Agente") -- Chat Message
+      end
+end)
