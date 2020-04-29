360337eb25494585a8474cbc69561adbfc4c0ebe
Vittorio D'Alfonso <50889805+GrandeVx@users.noreply.github.com>
2020-04-29 15:00:30 +0200

    Delete client.lua

--- Victory_SearchJob/client/client.lua
+++
@@ -1,6 +0,0 @@
-RegisterNetEvent("output") -- Client Event
-AddEventHandler("output", function(argument)  -- If Event Was Called
-      if argument == 'police' then  -- Check the User Job // argument = User Job
-    TriggerEvent("chatMessage", "[Server]", {0,255,0}, "Sei Un Agente") -- Chat Message
-      end
-end)
