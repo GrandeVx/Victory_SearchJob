6816834f14a409913b60c4ecffd88040ea6c3ff9
Vittorio D'Alfonso <50889805+GrandeVx@users.noreply.github.com>
2020-04-29 16:59:40 +0200

    Delete __resource.lua

--- Victory_SearchJob/__resource.lua
+++
@@ -1,8 +0,0 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-client_script "client/client.lua"
-
-server_scripts {
-    "client/server.lua",
-    "@mysql-async/lib/MySQL.lua"  -- Essensial
-}
